8cf520f4c40949a0299b9f33c69d61d7465be530
rockcor <59243884+rockcor@users.noreply.github.com>
2023-06-02 15:30:56 +0800

    Update Reproduce.sh

--- Reproduce.sh
+++ Reproduce.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# unzip data.zip
+# pip install -r requirement.yaml
+# run Reproduce.sh
 python main.py --dataset cora --lr 0.1 --wd 0.001 --dropout 0.8 --finaldp 0 --hops 1 --includeself 0 --addself 1 --threshold 2
 python main.py --dataset citeseer --lr 0.001 --wd 0 --dropout 0.8 --finaldp 0.9 --hops 2 --includeself 1 --addself 1 --threshold 2
 python main.py --dataset pubmed --lr 0.1 --wd 0.0001 --dropout 0.8 --finaldp 0.6 --hops 1 --includeself 0 --addself 1 --threshold 2.5
